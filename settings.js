c51cefc4451c129fc2cecdc91ea8f1486ffce24b
johanbottt <yassyouss.2009@gmail.com>
2025-02-25 16:10:05 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'johan',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "johan Bot",
+  botOwner: 'johan bot', // Your name
+  ownerNumber: '212774383575', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
